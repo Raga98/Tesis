896bfa02b3fbdd146ff425cf4276bcea9068f98f
unknown <oscar.272731@gmail.com>
2020-09-28 17:01:25 -0400

    Last changes

--- Tesis/src/app/tab2/tab2.page.ts
+++ Tesis/src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,16 +12,62 @@ export class Tab2Page {
 
   running = false;
   stopping = true;
+  constructor( private fb : FormBuilder, public toast : ToastController, public http : HttpClient) {}
 
-  constructor() {}
+  washForm = this.fb.group({
+    tamCarga0: ['', Validators.required],
+    tempLav0: ['', Validators.required],
+    tempEnj0: ['', Validators.required],
+    tipoLav0: ['', Validators.required],
+    configCiclo0: ['', Validators.required],
+  })
   //TODO: LOGICA DE CONTROL PARA PODER HACER SLIDE SOLO CUANDO SE HALLA HECHO CHECKED A UN RADIOBUTTON
 
   //TODO: ARRAY PARA LOS ELEMENTOS DE LAS DISTINTAS CONFIGURACIONES
   run() {
-    this.running = !this.running;
+    if(this.tamanoCarga.value == "" || this.tempLavado.value == "" || this.tipoLav.value == "" || this.tipoCiclo.value == ""){
+      console.log("Falta algun campo");
+      this.presentToast();
+    }
+    else {
+      let json = {
+        "tamanoCarga": this.tamanoCarga.value,
+        "tempLavado" : this.tempLavado.value,
+        "tipoLav" : this.tipoLav.value,
+        "tipoCiclo" : this.tipoCiclo.value,
+      };
+
+      this.running = !this.running;
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toast.create({
+      message: 'Le falta elegir una opcion',
+      duration: 2000,
+      position: 'top',
+    });
+    toast.present();
   }
   stop() {
+    console.log(this.tamanoCarga);
     this.stopping = !this.stopping;
+  }
+
+  get tamanoCarga(){
+    return this.washForm.get('tamCarga0');
+  }
+
+  get tempLavado(){
+    return this.washForm.get('tempLav0');
+  }
+
+  get tipoLav(){
+    return this.washForm.get('tipoLav0');
+  }
+
+  get tipoCiclo(){
+    return this.washForm.get('configCiclo0');
   }
 
 }
